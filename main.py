fc7a321d780472eb5bae5424f7f4e0d44e8a8d66
IsmaelBecker13 <ismaelbecker90@gmail.com>
2025-03-20 15:14:07 -0300

    1ercommitEj1

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from machine import Pin
 from time import sleep
 
-led_board = Pin("LED", Pin.OUT)
+led_board = Pin("LED", Pin.OUT)#Led Integrado
 sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
-    try:
-        led_board.toggle()
+    try: #Para apagar el programa con un ctrl + c
+        led_board.toggle()#Invierte el estado del led, no existe en esp32
         # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
-    except KeyboardInterrupt:
+        sleep(.5) # sleep 0.5 sec
+    except (KeyboardInterrupt):
         break
-led_board.off()
+led_board.off() #Apago el led, porque si estaba prendido quedaria asi
 print("Listo")
